49aa9f6227d80eaa276507f66878414be6a7e6bb
south boske <southboske@souths-MacBook-Pro.local>
2016-02-09 12:12:53 -0600

    Search bar

---
+++ InstaGram/SearchCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCollectionViewCell.swift
+//  InstaGram
+//
+//  Created by south boske on 2/9/16.
+//  Copyright © 2016 ksabbak. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
