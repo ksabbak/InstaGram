5c03d7ab1363fe202e1d833bc98d9a8d20a5e14d
K Sabbak <ksabbak@Hogwarts-2.local>
2016-02-10 05:44:08 -0600

    Set up framework for photouploads

--- InstaGram/CameraViewController.swift
+++ InstaGram/CameraViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
-class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate {
     
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var imageView: UIImageView!
+    var ref = Firebase(url: baseURL + "/photos")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        shareButton.enabled = false
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -41,12 +46,47 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
         let chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
         imageView.image = chosenImage
         picker.dismissViewControllerAnimated(true, completion: nil)
+        shareButton.enabled = true
         
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func onShareButtonTapped(sender: UIButton)
+    {
+        uploadPhoto()
+        imageView.image = nil
+        shareButton.enabled = false
+    }
+    
+    func uploadPhoto()
+    {
+        //let postRef = ref.childByAppendingPath("posts")
+        var aboutPhoto = ""
+        
+        if textView.text != "Tap to add description" && textView.text.isEmpty == false
+        {
+            aboutPhoto = textView.text
+        }
+        
+        
+        let post1 = ["userID": NSUserDefaults.standardUserDefaults().valueForKey("userID")!, "photo": 1, "about": aboutPhoto]
+        let post1Ref = ref.childByAutoId()
+        post1Ref.setValue(post1)
+    }
+
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n"
+        {
+            textView.resignFirstResponder()
+            return false
+        }
+        
+        return true
+    }
+
     
 }
 
