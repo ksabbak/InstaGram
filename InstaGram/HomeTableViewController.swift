89e050204eea1270cdbde8c3077ff11c57c85d82
K Sabbak <ksabbak@Hogwarts-2.local>
2016-02-10 13:47:37 -0600

    EVERYTHING ALWAYS

--- InstaGram/HomeTableViewController.swift
+++ InstaGram/HomeTableViewController.swift
@@ -7,89 +7,101 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeTableViewController: UITableViewController {
 
+    var photos = [PhotoPost]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.translucent = false
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        if NSUserDefaults.standardUserDefaults().valueForKey("userID") != nil && DataService.dataService.CURRENT_USER_REF.authData != nil {
+            //self.performSegueWithIdentifier("LoginSegue", sender: nil)
+        }
+        else
+        {
+            self.performSegueWithIdentifier("LoginSegue", sender: nil)
+        }
+        
+        let ref = Firebase(url: baseURL + "/photos")
+        
+//        ref.observeEventType(.Value, withBlock: { snapshot in
+//            println(snapshot.value)
+//            }, withCancelBlock: { error in
+//                println(error.description)
+//        })
+        
+        
+        ref.observeEventType(.Value, withBlock: { snapshot in
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                self.photos = []
+                
+                for snap in snapshots {
+                    let photoDictionary = snap.value as? Dictionary<String, AnyObject>
+                
+                    let newPhoto = PhotoPost(photoDictionary: photoDictionary!)
+                    
+                    self.photos.append(newPhoto)
+                }
+                   self.tableView.reloadData()
+            
+            }
+            }, withCancelBlock: { error in
+                                print(error.description)
+            })
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if photos.count > 0
+        {
+            return photos.count
+        }
+        
         return 0
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellID", forIndexPath: indexPath)
 
-        // Configure the cell...
+        if photos.count > 0
+        {
+            cell.imageView?.image = UIImage(data: photos[indexPath.row].photoPhoto)
+        }
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+   
+    
+    //MARK: - Prepare for Segue
+   
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailSegue"
+        {
+            let dvc = segue.destinationViewController as! DetailViewController
+            
+            let indexPath = self.tableView.indexPathForSelectedRow
+            
+            dvc.photo = photos[(indexPath?.row)!]
+            
+            
+            
+            
+        }
     }
-    */
 
+  
 }
