5d51d6475e13d174ef7d776e35713faad353ada7
Kanybek Zhagusaev <zhagusaev@gmail.com>
2016-02-10 13:31:38 -0600

    first merge

--- InstaGram/LoginViewController.swift
+++ InstaGram/LoginViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
-    
-    @IBOutlet weak var emailField: UITextField!
+class LoginViewController: UIViewController, UIActionSheetDelegate {
+    var authHelper: TwitterAuthHelper!
+    var accounts: [ACAccount]!
+    var ref: Firebase!
+        @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ref = Firebase(url:"https://instantgram.firebaseIO.com")
 
-        // Do any additional setup after loading the view.
+        authHelper = TwitterAuthHelper(firebaseRef:ref, apiKey: "l89MUHyl9PJMrEh6dwZldWouc")
     }
 
     @IBAction func tryLogin(sender: AnyObject) {
@@ -64,4 +68,83 @@ class LoginViewController: UIViewController {
         presentViewController(alert, animated: true, completion: nil)
     }
 
+    @IBAction func twitterButtonPressed(sender: UIButton) {
+        self.authWithTwitter()
+        
+    }
+    
+  
+    
+    func authWithTwitter() {
+        authHelper.selectTwitterAccountWithCallback { (error, accounts) -> Void in
+            self.accounts = accounts as! [ACAccount]
+            self.handleMultipleTwitterAccounts(self.accounts)
+        }
+    }
+    
+    func authAccount(account: ACAccount) {
+        authHelper.authenticateAccount(account, withCallback: { (error, authData) -> Void in
+            if error != nil {
+                // There was an error authenticating
+            } else {
+                // We have an authenticated Twitter user
+                NSLog("%@", authData)
+                // segue to chat
+//                self.performSegueWithIdentifier("twitterLogin", sender: authData)
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        })
+    }
+    
+    func selectTwitterAccount(accounts: [ACAccount]) {
+        let actionSheetController: UIAlertController = UIAlertController(title: "Select Twitter Account", message: "Swiftly Now! Choose an option!", preferredStyle: .ActionSheet)
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+        }
+//
+//        let selectUserActionSheet = UIActionSheet(title: "Select Twitter Account", delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: "Destruct", otherButtonTitles: "Other")
+        
+        for account in accounts {
+            let buttonTitle :UIAlertAction = UIAlertAction(title: account.username, style: .Default , handler: { (UIAlertAction) -> Void in
+                
+            })
+            actionSheetController.addAction(buttonTitle)
+        }
+        actionSheetController.addAction(cancelAction)
+//        actionSheetController.showInView(self.view);
+    }
+    
+    func handleMultipleTwitterAccounts(accounts: [ACAccount]) {
+        switch accounts.count {
+        case 0:
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://twitter.com/signup")!)
+        case 1:
+            self.authAccount(accounts[0])
+        default:
+            self.selectTwitterAccount(accounts)
+
+    }
+
+//    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+//        let currentTwitterHandle = actionSheet.buttonTitleAtIndex(buttonIndex)
+//        for acc in accounts {
+//            if acc.username == currentTwitterHandle {
+//                self.authAccount(acc)
+//            }
+//        }
+//        
+//    }
+    
+//    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//        let messagesVc = segue.destinationViewController as! HomeTableViewController
+//        if let authData = sender as? FAuthData {
+//            messagesVc.user = authData
+//            messagesVc.ref = ref
+//            messagesVc.sender = authData.providerData["username"] as! String
+//        }
 }
+
+}
+
+    
+
+
---
+++ InstaGram/TwitterAuthHelper.h
@@ -0,0 +1,35 @@
+//
+//  TwitterAuthHelper.h
+//  InstaGram
+//
+//  Created by Kanybek Zhagusaev on 2/9/16.
+//  Copyright © 2016 ksabbak. All rights reserved.
+//
+
+
+
+#import <Firebase/Firebase.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+
+@interface TwitterAuthHelper : NSObject
+
+@property (strong, nonatomic) ACAccountStore *store;
+@property (strong, nonatomic) Firebase *ref;
+@property (strong, nonatomic) NSString *apiKey;
+@property (strong, nonatomic) NSArray *accounts;
+
+- (id) initWithFirebaseRef:(Firebase *)ref apiKey:(NSString *)apiKey;
+
+// Step 1a
+- (void) selectTwitterAccountWithCallback:(void (^)(NSError *error, NSArray *accounts))callback;
+
+// Step 1b through 3:
+- (void) authenticateAccount:(ACAccount *)account withCallback:(void (^)(NSError *error, FAuthData *authData))callback;
+
+@end
+
+typedef NS_ENUM(NSInteger, AuthHelperError) {
+    AuthHelperErrorAccountAccessDenied = -1,
+    AuthHelperErrorOAuthTokenRequestDenied = -2
+};
